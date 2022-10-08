48111c605c05b63e144e36121d2d6fe421ab03a0
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-10-08 18:22:24 -0300

    input funcionando, falta validador

--- TP12ORGA.asm
+++ TP12ORGA.asm
@@ -8,12 +8,12 @@ section     .data
         
 
 section     .bss
-    cadena1     resw 20
-    cadena2     resw 20
-    cadena3     resw 20
-    cadena4     resw 20
-    cadena5     resw 20
-    cadena6     resw 20
+    cadena1     resw 40
+    cadena2     resw 40
+    cadena3     resw 40
+    cadena4     resw 40
+    cadena5     resw 40
+    cadena6     resw 40
 
 
 
@@ -21,27 +21,49 @@ section     .text
 main:
     mov rbp, rsp; for correct debugging
     mov rdx,1
-    inicio:
+    mov rcx,cadena1
+    call input
+    mov rdx,2
+    mov rcx,cadena2
+    call input
+    mov rdx,3
+    mov rcx,cadena3
+    call input
+    mov rdx,4
+    mov rcx,cadena4
+    call input
+    mov rdx,5
+    mov rcx,cadena5
+    call input
+    mov rdx,6
+    mov rcx,cadena6
+    call input
+    ret
+ 
+input:
+    ;rdx numero de ciclo
+    ;rcx donde se deja la cadena
+    mov rdi,rcx
     
     mov rcx,textCadena
     sub rsp,32
     call printf
     add rsp,32
     
-    mov rcx,cadena1
+    
+    mov rcx,rdi
+    
+    
     sub rsp,32
     call gets
     add rsp,32
     
-    inc rdx
-   
-    cmp rdx,6
-    jle inicio
-    call analyze
+    mov rcx,rdi
+    call validador
+    cmp rax,1;Si validador devuelve 1, es valido
+    jne input:
     ret
-input:
-    ;rax numero de ciclo
-    ;rcx donde se deja la cadena
+validador:
     
 
 analyze:
