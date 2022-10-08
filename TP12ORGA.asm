6cf3421ad684510756647d95d988c2ce893b0f39
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-10-08 19:16:36 -0300

    validador empezado, aunque no anda

--- TP12ORGA.asm
+++ TP12ORGA.asm
@@ -8,12 +8,12 @@ section     .data
         
 
 section     .bss
-    cadena1     resw 40
-    cadena2     resw 40
-    cadena3     resw 40
-    cadena4     resw 40
-    cadena5     resw 40
-    cadena6     resw 40
+    cadena1     resw 100
+    cadena2     resw 100
+    cadena3     resw 100
+    cadena4     resw 100
+    cadena5     resw 100
+    cadena6     resw 100
 
 
 
@@ -38,9 +38,10 @@ main:
     mov rdx,6
     mov rcx,cadena6
     call input
-    ret
+        ret
  
 input:
+    start:
     ;rdx numero de ciclo
     ;rcx donde se deja la cadena
     mov rdi,rcx
@@ -61,10 +62,47 @@ input:
     mov rcx,rdi
     call validador
     cmp rax,1;Si validador devuelve 1, es valido
-    jne input:
+    jne start
     ret
 validador:
+    mov r9,0;contador de elementos
+    mov r8,0;contador de bytes movidos
+    ;si me muevo mas de 60 bytes, deberia haber 20 elementos max
+    jmp startValidador
+    restart:
+    inc r8
+    inc r9
+    cmp r9,20
+    je endValidador
+    startValidador:
     
+    cmp rcx,32
+    je restart
+    inc rcx
+    inc r8
+    cmp rcx,32
+    je restart
+    inc rcx
+    inc r8
+    cmp rcx,32
+    je restart
+    
+    mov r9,100 ;r9 se convierte en la cantidad de bytes para llegar al final de la cadena reservada
+    sub r9,r8
+    
+    
+    endValidador:;Hay 3 elementos seguidos que no son espacio
+    dec r9
+    cmp rcx,0;si ese elemento (y todos los que le siguen) son espacio, se termino la cadena
+    jne errorValidador
+    inc rcx
+    cmp r9,0
+    jg endValidador
+    mov rax,1
+    ret
+    errorValidador:
+    mov rax, 0
+    ret
 
 analyze:
     ret
